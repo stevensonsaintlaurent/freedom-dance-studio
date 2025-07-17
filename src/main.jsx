880d3087590ea455daac9818f03e194c172c6508
St laurent Stevenson <stevensonstlaurent08@gmail.com>
2025-07-17 14:16:33 +0300

    what next

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode basename={import.meta.env.BASE_URL}>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+console.log(import.meta.env.BASE_URL);
