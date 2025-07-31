5ea9718a2aa68a09c6078b3b3c2dd7915b415bc0
St laurent Stevenson <stevensonstlaurent08@gmail.com>
2025-07-31 08:41:39 +0300

    videograph

--- src/components/Navbar/About/About.jsx
+++ src/components/Navbar/About/About.jsx
@@ -1,12 +1,12 @@
 import "./About.css";
-// import about_img from "../../../assets/poster.jpg";
+import about_img from "../../../assets/poster.jpg";
 import play_icon from "../../../assets/play-icon.png";
 
 const About = ({ setPlayState }) => {
   return (
     <div className="about">
       <div className="about-left">
-        {/* <img src={about_img} alt="" className="about-img" /> */}
+        <img src={about_img} alt="" className="about-img" />
         <img
           src={play_icon}
           alt=""
