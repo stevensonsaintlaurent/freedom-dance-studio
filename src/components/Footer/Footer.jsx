72362765e1f8998fe2e96d4b234a5e401677270f
St laurent Stevenson <stevensonstlaurent08@gmail.com>
2025-10-20 10:30:22 +0300

    update 2 for today

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <p> &#169; 2023 Freedom Dance Academy.All rights reserved</p>
+      <p> &#169; 2023 Freedom Dance Studio.All rights reserved</p>
       <ul>
         <li>Terms of Services</li>
         <li>Privacy Policy</li>
