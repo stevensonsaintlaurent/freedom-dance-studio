774ade50f35c29aed1cd2cd134bb8509e82224ab
St laurent Stevenson <stevensonstlaurent08@gmail.com>
2025-11-23 09:53:38 +0300

    bugsssssss

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Hero from "./components/Navbar/Hero/Hero";
 import Programs from "./components/Navbar/Programs/Programs";
 import Title from "./components/Navbar/Title/Title";
 import About from "./components/Navbar/About/About";
-// import Dance from "./components/Navbar/Dance/Dance";
-// import Testimonials from "./components/Navbar/Testimonials/Testimonials";
+import Dance from "./components/Navbar/Dance/Dance";
+import Testimonials from "./components/Navbar/Testimonials/Testimonials";
 import Contact from "./components/Navbar/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
@@ -20,10 +20,10 @@ const App = () => {
         <About setPlayState={setPlayState} />
         <Title subtitle="Our PROGRAM" title="What We Offer" />
         <Programs />
-        {/* <Title subtitle="Gallery" title="Dance Photos" /> */}
-        {/* <Dance /> */}
-        {/* <Title subtitle="TESTIMONIALS" title="What Dancers Says" />
-        <Testimonials /> */}
+        <Title subtitle="Gallery" title="Dance Photos" />
+        <Dance />
+        <Title subtitle="TESTIMONIALS" title="What Dancers Say" />
+        <Testimonials />
         <Title subtitle="Contact Us" title="Get in Touch" />
         <Contact />
         <Footer />
