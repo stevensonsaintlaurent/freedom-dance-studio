75792ea6ac8ad19fa755546e69e984813235446e
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-03-04 12:33:41 +0300

    add a new things to the form registration

--- src/components/Navbar/Contact/Contact.jsx
+++ src/components/Navbar/Contact/Contact.jsx
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Contact.css";
 import msg_icon from "../../../assets/msg-icon.png";
 import mail_icon from "../../../assets/mail-icon.png";
 import location_icon from "../../../assets/location-icon.png";
 import phone_icon from "../../../assets/phone-icon.png";
-import white_arrow from "../../../assets/white-arrow.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faTiktok } from "@fortawesome/free-brands-svg-icons";
+import RegistrationForm from "./RegistrationForm";
 
 const Contact = () => {
   const [result, setResult] = React.useState("");
+  const [hidden, setHidden] = React.useState(false);
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    setResult("Sending....");
+    setResult("Registering....");
+    setHidden(true);
     const formData = new FormData(event.target);
 
     formData.append("access_key", "3148a73d-e80f-42d9-a521-d4c3192cb2c7");
@@ -28,112 +30,100 @@ const Contact = () => {
     const data = await response.json();
 
     if (data.success) {
-      setResult("Form Submitted Successfully");
+      setResult(
+        "Thank you for registering! We have received your submission. If you have any questions or need assistance, you may contact us directly at freedomdancelasvegas@gmail.com or call/text us at 725-724-0962 for assistance",
+      );
       event.target.reset();
     } else {
-      console.log("Error", data);
       setResult(data.message);
     }
   };
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setResult("");
+      setHidden(false);
+    }, 10000);
+
+    return () => clearTimeout(timer);
+  }, [result]);
+
   return (
     <div className="contact">
+      {hidden ? null : (
+        <div className="contact-col">
+          <h3>
+            Send us a message <img src={msg_icon} alt="" />
+          </h3>
+          <p>
+            Feel free to reach out through contact form or find our contact
+            information bellow.Your feedback,questions,and suggestions are
+            important to us as we strive to provide exceptional service to our
+            Dance Company
+          </p>
+
+          <ul>
+            <li>
+              <a href="freedomdancelasvegas@gmail.com" className="link-item">
+                <img src={mail_icon} alt="" />
+                freedomdancelasvegas@gmail.com
+              </a>
+            </li>
+
+            <li>
+              <a href="7257240962" className="link-item">
+                <img src={phone_icon} alt="" />
+                7257240962
+              </a>
+            </li>
+
+            <li>
+              <a
+                href=" 3110 E Sunset Rd Suite C, Las Vegas, NV 89120"
+                className="link-item"
+              >
+                <img src={location_icon} alt="" /> 3110 E Sunset Rd Suite C, Las
+                Vegas, NV 89120
+              </a>
+            </li>
+
+            <li>
+              <a href="https://www.facebook.com/profile.php?id=100095031592137&mibextid=LQQJ4d">
+                <FontAwesomeIcon icon={faFacebook} /> Facebook
+              </a>
+            </li>
+
+            <li>
+              <a href="https://www.instagram.com/vegas_freedom_dance_studio?igsh=MXVoNmJtODh1azJ4Zg== ">
+                <FontAwesomeIcon icon={faInstagram} /> Instagram
+              </a>
+            </li>
+
+            <li>
+              <a href="https://www.tiktok.com/@fd.las_vegas?_t=8qEDieUuXCJ&_r=1">
+                <FontAwesomeIcon icon={faTiktok} /> Tiktok
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
+
       <div className="contact-col">
-        <h3>
-          Send us a message <img src={msg_icon} alt="" />
-        </h3>
-        <p>
-          Feel free to reach out through contact form or find our contact
-          information bellow.Your feedback,questions,and suggestions are
-          important to us as we strive to provide exceptional service to our
-          Dance Company
-        </p>
-
-        <ul>
-          <li>
-            <a href="freedomdancelasvegas@gmail.com" className="link-item">
-              <img src={mail_icon} alt="" />
-              freedomdancelasvegas@gmail.com
-            </a>
-          </li>
-
-          <li>
-            <a href="7257240962" className="link-item">
-              <img src={phone_icon} alt="" />
-              7257240962
-            </a>
-          </li>
-
-          <li>
-            <a
-              href=" 3110 E Sunset Rd Suite C, Las Vegas, NV 89120"
-              className="link-item"
-            >
-              <img src={location_icon} alt="" /> 3110 E Sunset Rd Suite C, Las
-              Vegas, NV 89120
-            </a>
-          </li>
-
-          <li>
-            <a href="https://www.facebook.com/profile.php?id=100095031592137&mibextid=LQQJ4d">
-              <FontAwesomeIcon icon={faFacebook} /> Facebook
-            </a>
-          </li>
-
-          <li>
-            <a href="https://www.instagram.com/vegas_freedom_dance_studio?igsh=MXVoNmJtODh1azJ4Zg== ">
-              <FontAwesomeIcon icon={faInstagram} /> Instagram
-            </a>
-          </li>
-
-          <li>
-            <a href="https://www.tiktok.com/@fd.las_vegas?_t=8qEDieUuXCJ&_r=1">
-              <FontAwesomeIcon icon={faTiktok} /> Tiktok
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="contact-col">
-        <h1>Register now for your first class free! </h1>
-
-        <form onSubmit={onSubmit}>
-          <label>Your name</label>
-          <input
-            type="text"
-            name="name"
-            placeholder=" Enter your name"
-            required
-          />
-
-          <label>Your Email</label>
-          <input
-            type="email"
-            name="email"
-            placeholder=" Enter your email Address"
-            required
-          />
-
-          <label>Phone Number</label>
-          <input
-            type="tel"
-            name="phone"
-            placeholder=" Enter your mobile number"
-            required
-          />
-
-          <label>Write your messages here</label>
-          <textarea
-            name="message"
-            rows="6"
-            placeholder="Enter your message "
-            required
-          ></textarea>
-          <button type="submit" className="btn dark-btn">
-            Register now <img src={white_arrow} alt="" />
-          </button>
-        </form>
-        <span>{result}</span>
+        {hidden === false ? (
+          <>
+            <h1>Register now for your first class free! </h1>
+            <RegistrationForm onSubmit={onSubmit} />
+          </>
+        ) : (
+          <span
+            style={{
+              fontSize: "28px",
+              fontWidth: "inherit",
+            }}
+          >
+            {result}
+          </span>
+        )}
       </div>
     </div>
   );
