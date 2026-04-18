f061610adf94eb7b7c3320d06710e58c57258285
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-04-18 19:29:54 +0300

    working on the testimonials slider from library react

--- src/components/Navbar/Contact/data.js
+++ src/components/Navbar/Contact/data.js
@@ -1,5 +1,5 @@
-import gabriela from "../../../assets/user-1.jpg";
-import zagir from "../../../assets/user-2.jpg";
+import gabriela from "../../../assets/gabriel.jpg";
+import zagir from "../../../assets/zagir.jpg";
 import allyson from "../../../assets/ally3.jpg";
 import maribel from "../../../assets/maribel.jpg";
 import adriana from "../../../assets/adriana.jpg";
@@ -37,48 +37,48 @@ export const dataRooms = [
   },
 ];
 
-const testimonials = [
+export const testimonials = [
   {
     id: 1,
     name: "Gabriela Oman",
     text: "The instructors at Freedom Dance are amazing. I'm so happyI found this company. Stevenson is very humble and patient. He encourages you to own your sexiness and motivates you to keep learning and coming back by always being so inclusive and friendly.",
-    image: { gabriela },
+    image: gabriela,
   },
 
   {
     id: 2,
     name: "Zagir",
     text: "The instructors are super patient and professional, they constantly motivate and challenge you to continue learning in a positive, respectful way, on your time and free of pressure. They completely understand that we are all different and learndifferently. In Freedom Dance I have found a family.",
-    image: { zagir },
+    image: zagir,
   },
   {
     id: 3,
     name: "Allyson Corella",
-    text: "   My first ever dance class was with Stevenson at Freedom Dance.  What I appreciate the most about Stevenson is he encourages diversity and inclusivity by suggesting students explore different instructors. My favorite part about class is the combination of learning and fun, plus the new friends I&apos;ve made along the way. I would encourage anyone who wants to find new joy in their life, to sign up for Bachata and Kizomba with Freedom Dance💓",
-    image: { allyson },
+    text: " My first ever dance class was with Stevenson at Freedom Dance.  What I appreciate the most about Stevenson is he encourages diversity and inclusivity by suggesting students explore different instructors. My favorite part about class is the combination of learning and fun, plus the new friends I&apos;ve made along the way. I would encourage anyone who wants to find new joy in their life, to sign up for Bachata and Kizomba with Freedom Dance💓",
+    image: allyson,
   },
   {
     id: 4,
     name: "Maribel Magana",
     text: "I started taking Bachata and Kizomba classes with Stevenson at Freedom Dance earlier this year. Stevenson is a very skilled and talented dancer who makes it his goal to encourage and teach his students to improve their dance skills while boosting their confidence. Not only is he a great teacher but his classes are also really fun. I highly recommend anyone who has a passion for dancing to go take his classes. You&apos;ll be learning from the best!",
-    image: { maribel },
+    image: maribel,
   },
   {
     id: 4,
     name: "Adriana Luna Blake",
     text: "I love attending Freedom Dance Company because the classes are fun. They focus on correcting and perfectioning the techniques, and Stevenson is very patient and professional with his students. They give each student the right amount of attention  and encouragement. I would highly recommend this dance studio.",
-    image: { adriana },
+    image: adriana,
   },
   {
     id: 5,
     name: "Kanani Jane",
     text: " Freedom Dance is actually where I fell in love with Kizomba. 💜 Just walked into Stevenson&apos;s class, knowing nothing about Kizomba, and BAMM!! Melted, lol. Later I took a Bachata Bootcamp and learned from a dancer from Romania. Such a beautiful dancer with so much skill and experience. I was blessed with being able to learn from her as well as Stevenson.",
-    image: { kanani },
+    image: kanani,
   },
   {
     id: 6,
     name: "Jesús Silva",
-    text: "               Freedom dance is one of the best dance studio in Vegas instructor Stevenson is a very nice and funny teacher He knows the dances he is teaching and puts all the enthusiasm to each class so come and join the team you will not regret it Personally I am very happy because there is a very good vibe with all the students come and practice Bachata and Kizomba.",
-    image: { jesus },
+    text: "Freedom dance is one of the best dance studio in Vegas instructor Stevenson is a very nice and funny teacher He knows the dances he is teaching and puts all the enthusiasm to each class so come and join the team you will not regret it Personally I am very happy because there is a very good vibe with all the students come and practice Bachata and Kizomba.",
+    image: jesus,
   },
 ];
