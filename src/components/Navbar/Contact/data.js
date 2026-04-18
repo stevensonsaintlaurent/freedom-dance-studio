6e3bdd1d195885266335558a6f7fce815e1c1634
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-04-18 17:56:46 +0300

    working on the testimonils

--- src/components/Navbar/Contact/data.js
+++ src/components/Navbar/Contact/data.js
@@ -1,3 +1,11 @@
+import gabriela from "../../../assets/user-1.jpg";
+import zagir from "../../../assets/user-2.jpg";
+import allyson from "../../../assets/ally3.jpg";
+import maribel from "../../../assets/maribel.jpg";
+import user_5 from "../../../assets/adriana.jpg";
+import user_6 from "../../../assets/kanani.jpg/";
+import user_7 from "../../../assets/jesus.jpg";
+
 export const dataRooms = [
   {
     id: 1,
@@ -26,5 +34,33 @@ export const dataRooms = [
       "The Both Rooms is a cozy and intimate space designed specifically for Kizomba classes and social dancing. It features a smooth dance floor, ambient lighting, and a sound system that creates the perfect atmosphere for Kizomba enthusiasts to connect and dance.",
     label: "both",
     value: "both",
+  },
+];
+
+const testimonials = [
+  {
+    id: 1,
+    name: "Gabriela Oman",
+    text: "The instructors at Freedom Dance are amazing. I'm so happyI found this company. Stevenson is very humble and patient. He encourages you to own your sexiness and motivates you to keep learning and coming back by always being so inclusive and friendly.",
+    image: { gabriela },
+  },
+
+  {
+    id: 2,
+    name: "Zagir",
+    text: "The instructors are super patient and professional, they constantly motivate and challenge you to continue learning in a positive, respectful way, on your time and free of pressure. They completely understand that we are all different and learndifferently. In Freedom Dance I have found a family.",
+    image: { zagir },
+  },
+  {
+    id: 3,
+    name: "Allyson Corella",
+    text: "   My first ever dance class was with Stevenson at Freedom Dance.  What I appreciate the most about Stevenson is he encourages diversity and inclusivity by suggesting students explore different instructors. My favorite part about class is the combination of learning and fun, plus the new friends I&apos;ve made along the way. I would encourage anyone who wants to find new joy in their life, to sign up for Bachata and Kizomba with Freedom Dance💓",
+    image: { allyson },
+  },
+  {
+    id: 4,
+    name: "Maribel Magana",
+    text: "   I started taking Bachata and Kizomba classes with Stevenson at Freedom Dance earlier this year. Stevenson is a very skilled and talented dancer who makes it his goal to encourage and teach his students to improve their dance skills while boosting their confidence. Not only is he a great teacher but his classes are also really fun. I highly recommend anyone who has a passion for dancing to go take his classes. You&apos;ll be learning from the best!",
+    image: { maribel },
   },
 ];
