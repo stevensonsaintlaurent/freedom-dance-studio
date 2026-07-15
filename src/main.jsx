97cc31f91a60cc1ef250e6ace822c4c29700b9b8
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-07-15 16:20:04 +0300

    getting better

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import App from "./App.jsx";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import AppD from "./AppD.jsx";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
 
     <AppD />
+    <ToastContainer position="top-center" autoClose={20000} />
   </StrictMode>,
 );
