17c1d25f16c5a8b113f1d7d66e0fd6ac66337fd5
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-07-19 22:50:45 +0300

    processing

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -4,7 +4,7 @@ const links = [
   { id: 1, url: "/", text: "home" },
   { id: 4, url: "about", text: "about" },
   { id: 2, url: "classes", text: "classes" },
-  { id: 2, url: "membership", text: "prices" },
+  { id: 8, url: "membership", text: "prices" },
   { id: 5, url: "contact", text: "contact" },
 ];
 
