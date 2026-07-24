9669c1dc0c904bb144390aaf60c3768e4797161a
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-07-24 04:59:03 +0300

    the website

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Hero } from "../components";
+import { Classes, StudioRental } from "../pages";
 
 const Landing = () => {
   return (
     <>
       <Hero />
+      <Classes />
+      <StudioRental />
     </>
   );
 };
